07a40a10490aa7c5df4b134207d27ff281050fac
Andrey <kindlebooks2022@mail.ru>
2022-09-11 05:33:12 +0400

    Seminar_3_1

--- Seminar/seminar_3/tesk_1.1/Program.cs
+++ Seminar/seminar_3/tesk_1.1/Program.cs
@@ -3,28 +3,33 @@
 находится эта точка. */
 
 
-int CheckChetvert(int x, int y)
-            {    
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int CheckChetvert(int x, int y)
+        {
             int res;
-            if (x>0 && y>0) res=1;
-            else if (x>0 && y<0) res=4;
-            else if (x<0 && y<0) res=3;
-            else if (x<0 && y>0) res=2;
+            if (x > 0 && y > 0) res = 1;
+            else if (x > 0 && y < 0) res = 4;
+            else if (x < 0 && y < 0) res = 3;
+            else if (x < 0 && y > 0) res = 2;
             else res = 0;
             return res;
-            }
-        
-        try 
-            {
-            Console.Write ("Введите координату х ");
-            int x = Convert.ToInt32 (Console.ReadLine());
-            Console.Write ("Введите координату y ");
-            int y = Convert.ToInt32 (Console.ReadLine());
-            int rez = CheckChetvert(x,y);
-            if (rez!=0)  Console.WriteLine($"Точка находится в четверти №  {rez}");
-            else Console.WriteLine("Точка находится лежит на какой-то оси");
-            }
+        }
+        try
+        {
+            Console.Write("Введите координату х ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите координату y ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            int rez = CheckChetvert(x, y);
+            if (rez != 0) Console.WriteLine($"Точка лежит в четверти №  {rez}");
+            else Console.WriteLine("Точка  лежит на какой-то оси");
+        }
         catch
-            {
-                Console.WriteLine("Надо было вводить именно целые числа");
-            }
+        {
+            Console.WriteLine("Надо было вводить именно целые числа");
+        }
+    }
+}
---
+++ Seminar/seminar_3/tesk_1.2/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 18:
+// Напишите программу, которая по заданому номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (X и Y).
+
+
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+
+        void fourth(int number)
+        {
+            if (number == 1)
+            {
+                Console.WriteLine("В первой четверти X может принимать значения от +0 до  +2.147.483.647.");
+                Console.WriteLine("В первой четверти Y может принимать значения от +0 до  +2.147.483.647.");
+            }
+            if (number == 2)
+            {
+                Console.WriteLine("В первой четверти X может принимать значения от +0 до +2.147.483.647.");
+                Console.WriteLine("В первой четверти Y может принимать значения от -1 до -2.147.483.648.");
+            }
+            if (number == 3)
+            {
+                Console.WriteLine("В первой четверти X может принимать значения от -1 до -2.147.483.648.");
+                Console.WriteLine("В первой четверти Y может принимать значения от -1 до -2.147.483.648.");
+            }
+            if (number == 4)
+            {
+                Console.WriteLine("В первой четверти X может принимать значения от +0 до +2.147.483.647.");
+                Console.WriteLine("В первой четверти Y может принимать значения от -1 до -2.147.483.648.");
+            }
+            if (number < 1 || number >4) 
+            {
+                Console.WriteLine("Всего может быть 4 координатные четверти от 1 до 4.");
+            }
+        }
+        
+        try
+        {
+            Console.Clear();
+            Console.WriteLine("Введите номер координатной четверти.");
+            int number = Convert.ToInt32(Console.ReadLine());
+            fourth(number);
+
+        }
+        catch
+        {
+            Console.WriteLine("Надо было вводить целое число.");
+        }
+        Console.WriteLine("Для выхода из программы нажмите ENTER.");
+        Console.ReadLine();
+
+
+
+    }
+}
